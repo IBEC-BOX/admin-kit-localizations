0de626e343256b6cf0d1383b859fb8a7d7bd321c
Bahataha <Bahataha@users.noreply.github.com>
2023-07-19 13:23:43 +0000

    Fix styling

--- src/UI/Filament/Resources/LocalizationResource/Pages/CreateLocalization.php
+++ src/UI/Filament/Resources/LocalizationResource/Pages/CreateLocalization.php
@@ -23,12 +23,11 @@ class CreateLocalization extends CreateRecord
 
     public function beforeCreate()
     {
-        foreach (config('admin-kit.locales') as $value){
-            $file = lang_path($value . '.json');
-            if (!file_exists($file)){
+        foreach (config('admin-kit.locales') as $value) {
+            $file = lang_path($value.'.json');
+            if (! file_exists($file)) {
                 file_put_contents($file, json_encode([$this->data['key'] => $this->data['content'][$value]], JSON_UNESCAPED_UNICODE));
-            }
-            else{
+            } else {
                 $fileOpened = file_get_contents($file);
                 $fullData = json_decode($fileOpened);
                 $key = $this->data['key'];
