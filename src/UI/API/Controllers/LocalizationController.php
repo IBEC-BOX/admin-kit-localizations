33eba73ce251f7f35852b309b5b64d9cddc05891
daurensky <dkambarov17@gmail.com>
2024-04-09 09:49:12 +0500

    feat: add language filter to api

--- src/UI/API/Controllers/LocalizationController.php
+++ src/UI/API/Controllers/LocalizationController.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace AdminKit\Localizations\UI\API\Controllers;
 
 use AdminKit\Localizations\Models\Localization;
+use AdminKit\Localizations\UI\API\Data\LocalizationData;
 
 class LocalizationController extends Controller
 {
     public function index()
     {
-        return Localization::all();
+        return LocalizationData::collection(Localization::all());
     }
 
     public function show(int $id)
---
+++ src/UI/API/Data/LocalizationData.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AdminKit\Localizations\UI\API\Data;
+
+use AdminKit\Localizations\Models\Localization;
+use Spatie\LaravelData\Data;
+
+class LocalizationData extends Data
+{
+    public function __construct(
+        public string $key,
+        public array|string $content,
+    ) {
+    }
+
+    public static function fromModel(Localization $localization): LocalizationData
+    {
+        $content = $localization->getTranslations('content');
+
+        if ($locale = request('locale')) {
+            $content = $content[$locale] ?? $content;
+        }
+
+        return new self(
+            key: $localization->key,
+            content: $content,
+        );
+    }
+}
